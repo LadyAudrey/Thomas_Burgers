40eba76ee4aab7dc7414a487019ba36f10b82c5a
LadyAudrey <theaudreygreene@gmail.com>
2023-10-12 18:05:57 -0700

    adding TW theme and confirmed is working, deleted default main

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,18 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      "darkBrown": "#303030",
+      "lightBrown": "#795833",
+      "brownOrange": "#e16539",
+      "orange": "#ffa857",
+      "pinkOrange": "#ff6b57",
+      "pink": "#ffb1a5",
+      "blue1": "#51abb2",
+      "blue2": "#6ec6cd",
+      "offWhite": "#f0dfc8"
+
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
