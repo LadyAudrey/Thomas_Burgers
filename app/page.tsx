3eb0d3df1fac2c5d63d8ccdca74da11bcbfdabf9
LadyAudrey <theaudreygreene@gmail.com>
2023-10-14 18:17:50 -0700

    Header pieces in place, needs links hooked up

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main className="h-screen w-screen bg-pink"></main>;
+  return (
+    <main className="h-screen w-screen">
+      <h1>filler</h1>
+    </main>
+  );
 }
