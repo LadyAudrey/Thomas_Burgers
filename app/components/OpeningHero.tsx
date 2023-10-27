baa561cd0c56bd9ac466584142d8cdc04d0be962
LadyAudrey <theaudreygreene@gmail.com>
2023-10-27 10:46:07 -0700

    hero css

--- app/components/OpeningHero.tsx
+++ app/components/OpeningHero.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export function OpeningHero() {
   return (
-    <div className="hero">
-      <div className="flex justify-center w-1/2 bg-lightBrown">
+    <div className="hero flex flex-col sm:flex-row w-full">
+      <div className="flex justify-center sm:w-1/2 bg-lightBrown">
         <Image
           src={"/misc/flyingBurger.png"}
           // TDL- want to figure out what needs to happen for fill
@@ -13,13 +13,13 @@ export function OpeningHero() {
           alt=""
         />
       </div>
-      <div className="flex flex-col  items-center justify-center w-1/2 bg-blue1">
+      <div className="flex flex-col  items-center justify-center sm:w-1/2 bg-blue1">
         <div className="flex flex-col  items-center">
-          <h1 className="text-6xl">Thomas Burger</h1>
+          <h1 className="text-6xl text-center">Thomas Burger</h1>
           <Link href="#">
             <h3 className="my-4">Ready to Order?</h3>
           </Link>
-          <button className="bg-orange roundBtn">Order Now!</button>
+          <button className="bg-orange roundBtn m-5">Order Now!</button>
         </div>
       </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,13 @@ import { Eyecandy } from "./components/Eyecandy";
 import Image from "next/image";
 
 export default function Home() {
- creating-feature-header
   return (
     <div className="h-full w-screen">
-      <OpeningHero />
+      {/* <OpeningHero />
       <Specials />
       <Menu />
       <BusinessInfo />
-      <Eyecandy />
+      <Eyecandy /> */}
     </div>
   );
-
 }
