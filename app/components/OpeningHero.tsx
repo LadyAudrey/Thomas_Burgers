045f6f1f34a9fbb6bed00d95b94f58a32028e5ad
Kenson Johnson <kenson.johnson@outlook.com>
2023-11-02 11:20:02 -0500

    Adjusted for tablet size viewing

--- app/components/OpeningHero.tsx
+++ app/components/OpeningHero.tsx
@@ -4,16 +4,17 @@ import Link from "next/link";
 export function OpeningHero() {
   return (
     <div className="hero flex flex-col sm:flex-row w-full">
-      <div className="flex justify-center sm:w-1/2 h-[600px] bg-lightBrown">
+      <div className="flex justify-center h-[600px] w-full bg-lightBrown">
         <Image
           src={"/misc/flyingBurger.png"}
           // TDL- want to figure out what needs to happen for fill
           height={550}
           width={400}
+          className="object-contain"
           alt=""
         />
       </div>
-      <div className="flex flex-col items-center justify-center sm:w-1/2 py-10 bg-blue1">
+      <div className="flex flex-col items-center justify-center w-full py-10 bg-blue1">
         <div className="flex flex-col items-center">
           <h1 className="text-6xl text-center">Thomas Burger</h1>
           <img src="/misc/YellowStripe.svg" className="w-1/2" />
