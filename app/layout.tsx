c542a855462ed49e8305e222c5377cfb326d164d
LadyAudrey <theaudreygreene@gmail.com>
2023-11-06 08:19:00 -0800

    fixed background color and width of app

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="w-screen bg-white">
       <body className="max-w-[1280px] mx-auto">
         <Header />
         {children}
