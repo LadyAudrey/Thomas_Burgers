42473915f080dd9d8471f406de2802ee07687337
LadyAudrey <theaudreygreene@gmail.com>
2023-11-06 08:23:44 -0800

    styling components
    hard coded max w/h
    updated file names from misc to images
    fix arrow SVG

--- app/components/SpecialItem.tsx
+++ app/components/SpecialItem.tsx
@@ -9,21 +9,33 @@ type SpecialItem = {
 export function SpecialItem(props: SpecialItem) {
   const { picture, title } = props;
   return (
-    <div className="special mx-5">
-      <div>
-        <Image
-          src={picture}
-          // TDL- want to figure out what needs to happen for fill
-          width={400}
-          height={400}
-          alt=""
-        />
-      </div>
-      <div className="specialBanner bg-brownOrange">
+    <div className="special h-96 w-96">
+      <img
+        src={picture}
+        // TDL- want to figure out what needs to happen for fill
+        width={384}
+        height={384}
+        alt=""
+        className="object-cover h-full w-full overflow-hidden"
+      />
+      <div className="flex flex-row rounded-b-lg justify-around place-items-center bg-brownOrange">
         <h3>{title}</h3>
         {/* TDL- make svg work and accept props of links*/}
         <Link href="#">
-          <button className="roundBtn bg-['misc/Arrow.svg')]">Arrow</button>
+          <svg
+            width="101"
+            height="55"
+            viewBox="0 0 101 55"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-15 h-8 my-2"
+          >
+            <path d="M0 27H90.5" stroke="#F0DFC8" stroke-width="8" />
+            <path
+              d="M99.8884 26.9984L52.444 50.8956L51.9383 4.13295L99.8884 26.9984Z"
+              fill="#F0DFC8"
+            />
+          </svg>
         </Link>
       </div>
     </div>
