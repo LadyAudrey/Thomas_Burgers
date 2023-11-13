544ee34c5dcea4057abd5ec2434f1da38d83406b
LadyAudrey <theaudreygreene@gmail.com>
2023-11-13 09:33:47 -0800

    added white to config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,32 +1,32 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     colors: {
-      "darkBrown": "#303030",
-      "lightBrown": "#795833",
-      "brownOrange": "#e16539",
-      "orange": "#ffa857",
-      "pinkOrange": "#ff6b57",
-      "pink": "#ffb1a5",
-      "blue1": "#51abb2",
-      "blue2": "#6ec6cd",
-      "offWhite": "#f0dfc8"
-
+      darkBrown: "#303030",
+      lightBrown: "#795833",
+      brownOrange: "#e16539",
+      orange: "#ffa857",
+      pinkOrange: "#ff6b57",
+      pink: "#ffb1a5",
+      blue1: "#51abb2",
+      blue2: "#6ec6cd",
+      offWhite: "#f0dfc8",
+      white: "#fff",
     },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
