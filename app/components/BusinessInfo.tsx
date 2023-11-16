cee12c3467b3d3033e87292bf910fb3b25411b4e
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2023-11-16 13:04:12 -0600

    Cleaned up unused classNames
    - removed button from inside link (invalid html)
    - halved padding value
    - centered text for times
    - adjusted spacing for icons

--- app/components/BusinessInfo.tsx
+++ app/components/BusinessInfo.tsx
@@ -3,14 +3,11 @@ import Link from "next/link";
 
 export function BusinessInfo() {
   return (
-    <div className="flex flex-col sm:flex-row items-center p-20 bg-darkBrown text-offWhite">
-      <div className="flex flex-col h-1/2 w-1/2">
-        <h2 className="text-3xl">
-          Thomas
-          <br />
-          Burger
-        </h2>
-        <p className="pt-4 gap-4">
+    <div className="flex flex-col sm:flex-row items-center sm:items-stretch p-10 bg-darkBrown text-offWhite">
+      {/* Left side */}
+      <div className="flex flex-col mb-2 text-center sm:text-start sm:w-1/2">
+        <h2 className="text-3xl">Thomas Burger</h2>
+        <p className="pt-4">
           Location:
           <br />
           12125 Navy St
@@ -18,17 +15,20 @@ export function BusinessInfo() {
           LA CA 90066
         </p>
       </div>
-      <div className="flex flex-col h-1/2 w-1/2 my-5 gap-4 whitespace-nowrap">
-        <div>
-          <p>Mon - Friday 8:00 AM - 10:00 PM</p>
-          <p>Sat - Sunday 8:00 AM - 10:00 PM</p>
+
+      {/* Right side */}
+      <div className="flex flex-col text-center sm:w-1/2 gap-4 whitespace-nowrap">
+        <div className="flex flex-col md:flex-row md:justify-between">
+          <p className="flex flex-col mb-2">
+            Mon - Friday <span>8:00 AM - 10:00 PM</span>
+          </p>
+          <p className="flex flex-col">
+            Sat - Sunday <span>8:00 AM - 10:00 PM</span>
+          </p>
         </div>
-        <div className="flex flex-col sm:flex-row justify-between">
-          <Link href="#">
-            {" "}
-            <button className="bg-orange text-darkBrown roundBtn my-10">
-              Get In Touch
-            </button>
+        <div className="flex flex-col sm:flex-row justify-between gap-5 items-center">
+          <Link href="#" className="bg-orange text-darkBrown roundBtn my-5">
+            Get In Touch
           </Link>
           <div className="flex flex-row gap-5">
             <Link href="#">
