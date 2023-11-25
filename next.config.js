e01fd1ccabe0e11044bef58347b5dea7e3b393fb
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2023-11-25 10:38:51 -0600

    Handle basePath during production build

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+// Sets the base path when running in production, needed for GitHub Pages
+// Should be removed when deploying to a custom domain
+const basePath = process.env.NODE_ENV === "production" ? "/thomas_burgers" : "";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
@@ -5,6 +9,7 @@ const nextConfig = {
     loader: "custom",
     loaderFile: "./loaderFile.ts",
   },
+  basePath,
 };
 
 module.exports = nextConfig;
